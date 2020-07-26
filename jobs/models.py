8f55e053b61bc81eca75601dcc9dbc06ef88605d
MD IRFAN <irfan02md@gmail.com>
2020-07-26 13:02:05 +0530

    jobs app model configuration done

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Jobs(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=30)
+    discription = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
